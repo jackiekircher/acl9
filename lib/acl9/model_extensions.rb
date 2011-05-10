3afd474f09024caff96fbb809c6df45e08deefdc
jackie kircher <kircher.jackie@gmail.com>
2011-05-10 16:45:28 -0700

    Edited lib/acl9/model_extensions.rb via GitHub

--- lib/acl9/model_extensions.rb
+++ lib/acl9/model_extensions.rb
@@ -38,7 +38,7 @@ module Acl9
         join_table = options[:join_table_name] || Acl9::config[:default_join_table_name] ||
                     join_table_name(undecorated_table_name(self.to_s), undecorated_table_name(role))
 
-        has_many :roles, :through => join_table
+        has_many :roles, :through => :roles_users
 
         cattr_accessor :_auth_role_class_name, :_auth_subject_class_name,
                        :_auth_role_assoc_name
